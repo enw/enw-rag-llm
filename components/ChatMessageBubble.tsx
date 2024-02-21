f352726b8c385bf1fde1966256613c8bc7db482c
Elliot Winard <elliot.winard@gmail.com>
2024-02-21 18:43:56 -0500

    use green a green color palette

--- components/ChatMessageBubble.tsx
+++ components/ChatMessageBubble.tsx
@@ -2,7 +2,7 @@ import type { Message } from "ai/react";
 
 export function ChatMessageBubble(props: { message: Message, aiEmoji?: string, sources: any[] }) {
   const colorClassName =
-    props.message.role === "user" ? "bg-sky-600" : "bg-slate-50 text-black";
+    props.message.role === "user" ? "bg-green-600" : "bg-slate-50 text-black";
   const alignmentClassName =
     props.message.role === "user" ? "ml-auto" : "mr-auto";
   const prefix = props.message.role === "user" ? "🧑" : props.aiEmoji;
