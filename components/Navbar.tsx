cf9d42217be679bf3e959300398ea42dab3b0948
Elliot Winard <elliot.winard@gmail.com>
2024-02-21 14:54:57 -0500

    [bug, fix build] properly escape a single quote

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,11 +6,11 @@ export function Navbar() {
   const pathname = usePathname();
   return (
     <nav className="mb-4">
-      <a className={`mr-4 ${pathname === "/" ? "text-white border-b" : ""}`} href="/">🐶 Elliot's Retrieval Bot</a>
+      <a className={`mr-4 ${pathname === "/" ? "text-white border-b" : ""}`} href="/">🐶 Elliot&apos;s Retrieval Bot</a>
       {/* <a className={`mr-4 ${pathname === "/chat" ? "text-white border-b" : ""}`} href="/chat">🏴‍☠️ Chat</a> */}
       {/* <a className={`mr-4 ${pathname === "/structured_output" ? "text-white border-b" : ""}`} href="/structured_output">🧱 Structured Output</a> */}
       {/* <a className={`mr-4 ${pathname === "/agents" ? "text-white border-b" : ""}`} href="/agents">🦜 Agents</a> */}
-      <a className={`mr-4 ${pathname === "/retrieval_agents" ? "text-white border-b" : ""}`} href="/retrieval_agents">🤖 Elliot's Retrieval Agent Bot</a>
+      <a className={`mr-4 ${pathname === "/retrieval_agents" ? "text-white border-b" : ""}`} href="/retrieval_agents">🤖 Elliot&apos;s Retrieval Agent Bot</a>
     </nav>
   );
 }
